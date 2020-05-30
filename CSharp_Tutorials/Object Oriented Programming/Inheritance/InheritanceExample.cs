a6d59cf91bc325ac33f618a5150d33869f789d9c
john harold <johnexxxcube@gmail.com>
2020-05-30 21:18:14 +0800

    Updated 5/30/2020 9:18PM

---
+++ CSharp_Tutorials/Object Oriented Programming/Inheritance/InheritanceExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+// InheritanceExample -- Provide the simplest possible
+// demonstration of inheritance.
+namespace InheritanceExample
+{
+	class BaseClass
+	{
+		public int _dataMember;
+		public void SomeMethod()
+		{
+			Console.WriteLine("SomeMethod()");
+		}
+	}
+	class SubClass : BaseClass // This is an exaple of inheritance.
+	{                          // means the SubClass inherit the BaseClass
+		public void SomeOtherMethod()
+		{
+			Console.WriteLine("SomeOtherMethod()");
+		}
+	}
+	class AnotherSubclass : BaseClass
+	{
+		public void AnotherSomeMethod()
+		{
+			Console.WriteLine("AnotherSomeMethod()");
+		}
+	}
+	public class InheritanceExampleClass
+	{
+		public static void InheritanceExampleMain()
+		{
+			// Create a base class object.
+			Console.WriteLine("Exercising a base class object:");
+			BaseClass bc = new BaseClass();
+			bc._dataMember = 1;
+			bc.SomeMethod();
+			// Now create a subclass object.
+			Console.WriteLine("Exercising a subclass object:");
+			SubClass sc = new SubClass();
+			sc._dataMember = 2;
+			sc.SomeMethod();
+			sc.SomeOtherMethod();
+			// Wait for user to acknowledge the results.
+			Console.WriteLine("Press Enter to terminate...");
+			Console.Read();
+		}
+	}
+}
--- CSharp_Tutorials/Program.cs
+++ CSharp_Tutorials/Program.cs
@@ -4,8 +4,10 @@ using CSharp_Tutorials.Object_Oriented_Programming;
 using CSharp_Tutorials.Object_Oriented_Programming.Holding_a_Class_Responsible;
 using CSharp_Tutorials.Object_Oriented_Programming.Let_Me_Say_About_this;
 using CSharp_Tutorials.Object_Oriented_Programming.We_Have_Our_Methods;
+using InheritanceExample;
 using System;
 using System.Runtime.InteropServices;
+using SimpleSavingsAccount;
 
 namespace MyNameSpace
 {
@@ -18,12 +20,12 @@ namespace MyNameSpace
             //int.TryParse(s, out int x);
             //string t = Console.ReadLine();
             //int.TryParse(t, out int y);                              
-            int[] temp = CodeExecises.MultiplyElements(new int[] { 1, 2, 3, 4, 5 },
-                                      new int[] { 1, 2, 3, 4, 5 });
-            foreach (var item in temp)
-            {
-                Console.Write("{0} ",item);
-            }
+            //int[] temp = CodeExecises.MultiplyElements(new int[] { 1, 2, 3, 4, 5 },
+            //                          new int[] { 1, 2, 3, 4, 5 });
+            //foreach (var item in temp)
+            //{
+            //    Console.Write("{0} ",item);
+            //}
             #region For Introduction Class Example
             //string[] s = new string[1];
             //s[0] = "D:\\Teaching Purposes\\dmc\\Other Projects 2019-2020\\Graduation Design";
@@ -60,8 +62,8 @@ namespace MyNameSpace
             //double x = Convert.ToDouble(Console.ReadLine());
             //double y = Convert.ToDouble(Console.ReadLine());
             //Methods.DisplayRation(x,y);
-            //DemonstrateCustomConstructor.DemonstrateCustomConstructorProg();
+            SimpleSavingsAccountClass.SimpleSavingsAccountClassMain();
             #endregion
-        }
+        } 
     }
 }
